5f22ef3eaa2293f261e76ef3c71bbbc84a4f8710
TMA-Git <125268517+TMA-Git@users.noreply.github.com>
2023-08-15 11:47:39 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,12 +7,11 @@ cd /home/docker/actions-runner
 ./config.sh --url https://github.com/$ORGANIZATION --token $REG_TOKEN
 
 cleanup() {
-    echo "Removing runner..."
-    ./config.sh remove --unattended --token $REG_TOKEN
+    echo "Removing runner..."
+    ./config.sh remove --unattended --token $REG_TOKEN
 }
 
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 
 ./run.sh & sudo ./svc.sh install & sudo ./svc.sh start & wait $!
-
