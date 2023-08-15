e5cfd3bc98b117909e2a497ac3303baba6b01ef3
TMA-Git <125268517+TMA-Git@users.noreply.github.com>
2023-08-15 12:17:16 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
+cd /home/docker/actions-runner
 REG_TOKEN=$(curl -sX POST -H "Authorization: token $ACCESS_TOKEN" https://api.github.com/orgs/$ORGANIZATION/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/$ORGANIZATION --token $REG_TOKEN
+if [ ! -f ".runner" ]; then
+    ./config.sh --url https://github.com/$ORGANIZATION --token $REG_TOKEN --name DEV_DOCKER_RUNNER
+fi
 
 cleanup() {
     echo "Removing runner..."
@@ -14,4 +16,11 @@ cleanup() {
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 
-./run.sh & wait $!
+# Install the runner as a service
+sudo ./svc.sh install
+
+# Start the runner service
+sudo ./svc.sh start
+
+# Keep the script running
+tail -f /dev/null
