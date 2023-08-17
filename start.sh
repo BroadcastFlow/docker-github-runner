8850a4cc530d18b3913236779a183a40508bf026
Jordan Hall <Jordan@libertyware.co.uk>
2023-08-17 11:32:03 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ cd /home/docker/actions-runner
 REG_TOKEN=$(curl -sX POST -H "Authorization: token $ACCESS_TOKEN" https://api.github.com/orgs/$ORGANIZATION/actions/runners/registration-token | jq .token --raw-output)
 
 if [ ! -f ".runner" ]; then
-    ./config.sh --url https://github.com/$ORGANIZATION --token $REG_TOKEN --name $RUNNER_NAME
+    ./config.sh --url https://github.com/$ORGANIZATION --token $REG_TOKEN --name $RUNNER_NAME --ephemeral
 fi
 
 cleanup() {
